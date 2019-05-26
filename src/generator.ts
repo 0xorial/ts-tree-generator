76ef8e9dbed559b3377b6fdc7201e8b000f519e3
0xorial <0xorial@gmail.com>
2019-05-26 13:18:49 +0200

    added root generation.

--- src/generator.ts
+++ src/generator.ts
@@ -3,6 +3,7 @@ import sr = require("seedrandom");
 export interface GenerateOptions {
   count: number;
   seed: string;
+  makeRoot: boolean;
 }
 
 export interface UsedReference {
@@ -16,6 +17,11 @@ export interface File {
   exports: Array<{ name: string }>;
   referencedBy: Array<File>;
   references: Array<UsedReference>;
+}
+
+export interface Invocation {
+  name: string;
+  file: File;
 }
 
 export function generate(options: GenerateOptions): ReadonlyArray<File> {
@@ -32,25 +38,44 @@ export function generate(options: GenerateOptions): ReadonlyArray<File> {
     r.push(fullFile);
     fullFile.references.forEach(x => x.file.referencedBy.push(fullFile));
   }
+
+
+  if (options.makeRoot) {
+    const refs = r
+      .filter(x => x.referencedBy.length === 0)
+      .map(x => ({ file: x, name: x.exports[0].name }));
+    const file = makeFileWithInvocations(refs, 1);
+    const fullFile = {
+      ...file,
+      relativePath: "./test-root.ts",
+      referencedBy: []
+    };
+    r.push(fullFile);
+  }
+
+
   return r;
+
 
   function randomInt(max: number) {
     return Math.floor(rng() * max);
   }
 
-  function makeFile() {
-    const length = Math.max(1, randomInt(20));
+  function makeFile(maxFunctions = 21) {
     const referencesCount = randomInt(Math.min(15, r.length));
-
-    const invocations: Array<{ name: string; file: File }> = [];
+    const invocations: Array<Invocation> = [];
     for (let i = 0; i < referencesCount; i++) {
       // reference files in the beginning more often, giving something similar to Paretto distribution
-      const fileIndex = Math.floor(Math.pow(randomInt(r.length), 0.4));
+      const fileIndex = Math.floor(randomInt(r.length));
       const file = r[fileIndex];
       const fn = file.exports[randomInt(file.exports.length)];
       invocations.push({ file, name: fn.name });
     }
 
+    return makeFileWithInvocations(invocations, maxFunctions);
+  }
+
+  function makeFileWithInvocations(invocations: Invocation[], maxFunctions: number) {
     const groupedInvocations = invocations.reduce<{
       [relativePath: string]: {
         file: File;
@@ -76,6 +101,7 @@ export function generate(options: GenerateOptions): ReadonlyArray<File> {
     const functions = [];
     const newFunctions = [];
     const calls = invocations.map(x => x.name);
+    const length = Math.max(1, randomInt(maxFunctions));
     for (let i = 0; i < length; i++) {
       const name = "fn" + randomInt(10000000);
       newFunctions.push(name);
